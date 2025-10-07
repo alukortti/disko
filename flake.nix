f6dc41f28f5a202de66d7e472879c069807b7e8c
alukortti <aleksi.koskinen91@gmail.com>
2025-10-07 22:08:03 +0300

    Add NixOS configuration with Btrfs and impermanence
    This file sets up a NixOS configuration with Btrfs support and impermanence features.

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "NixOS configuration with Btrfs and impermanence";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    impermanence.url = "github:nix-community/impermanence";
+  };
+
+  outputs = { self, nixpkgs, impermanence, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          impermanence.nixosModules.impermanence
+        ];
+      };
+    };
+}
