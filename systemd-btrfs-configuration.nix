ac1f617a09c48e510c91ad2166211a4f114a95ce
alukortti <aleksi.koskinen91@gmail.com>
2025-10-07 22:44:47 +0300

    Refactor Btrfs filesystem configuration for partitions

--- systemd-btrfs-configuration.nix
+++ systemd-btrfs-configuration.nix
@@ -10,56 +10,59 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  # Filesystems (matching disko.nix)
-  fileSystems."/" = {
-    device = "/dev/disk/by-id/virtio-zfsdisk0";
-    fsType = "btrfs";
-    options = [ "subvol=@root" "compress=zstd" "ssd" "discard=async" "relatime" ];
+  # Filesystems (compact format)
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-id/virtio-zfsdisk0-part2";
+      fsType = "btrfs";
+      options = [ "subvol=@root" "compress=zstd" ];
+    };
+
+    "/nix" = {
+      device = "/dev/disk/by-id/virtio-zfsdisk0-part2";
+      fsType = "btrfs";
+      options = [ "subvol=@nix" "compress=zstd" "noatime" ];
+      neededForBoot = true;
+    };
+
+    "/var/log" = {
+      device = "/dev/disk/by-id/virtio-zfsdisk0-part2";
+      fsType = "btrfs";
+      options = [ "subvol=@var_log" "compress=zstd" ];
+      neededForBoot = true;
+    };
+
+    "/var/lib" = {
+      device = "/dev/disk/by-id/virtio-zfsdisk0-part2";
+      fsType = "btrfs";
+      options = [ "subvol=@var_lib" "compress=zstd" ];
+      neededForBoot = true;
+    };
+
+    "/etc/nixos" = {
+      device = "/dev/disk/by-id/virtio-zfsdisk0-part2";
+      fsType = "btrfs";
+      options = [ "subvol=@etc_nixos" "compress=zstd" ];
+      neededForBoot = true;
+    };
+
+    "/persistent" = {
+      device = "/dev/disk/by-id/virtio-zfsdisk0-part2";
+      fsType = "btrfs";
+      options = [ "subvol=@persistent" "compress=zstd" ];
+      neededForBoot = true;
+    };
+
+    "/boot/efi" = {
+      device = "/dev/disk/by-id/virtio-zfsdisk0-part1";
+      fsType = "vfat";
+    };
   };
 
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-id/virtio-zfsdisk0";
-    fsType = "btrfs";
-    options = [ "subvol=@nix" "compress=zstd" "ssd" "discard=async" "relatime" ];
-    neededForBoot = true;
-  };
-
-  fileSystems."/var/log" = {
-    device = "/dev/disk/by-id/virtio-zfsdisk0";
-    fsType = "btrfs";
-    options = [ "subvol=@var_log" "compress=zstd" "ssd" "discard=async" "relatime" ];
-    neededForBoot = true;
-  };
-
-  fileSystems."/var/lib" = {
-    device = "/dev/disk/by-id/virtio-zfsdisk0";
-    fsType = "btrfs";
-    options = [ "subvol=@var_lib" "compress=zstd" "ssd" "discard=async" "relatime" ];
-    neededForBoot = true;
-  };
-
-  fileSystems."/etc/nixos" = {
-    device = "/dev/disk/by-id/virtio-zfsdisk0";
-    fsType = "btrfs";
-    options = [ "subvol=@etc_nixos" "compress=zstd" "ssd" "discard=async" "relatime" ];
-    neededForBoot = true;
-  };
-
-  fileSystems."/persistent" = {
-    device = "/dev/disk/by-id/virtio-zfsdisk0";
-    fsType = "btrfs";
-    options = [ "subvol=@persistent" "compress=zstd" "ssd" "discard=async" "relatime" ];
-    neededForBoot = true;
-  };
-
-  fileSystems."/boot/efi" = {
-    device = "/dev/disk/by-id/virtio-zfsdisk0-part1";
-    fsType = "vfat";
-  };
-
+  # Optional: impermanence-style root recreation before mount
   boot.initrd.postDeviceCommands = lib.mkAfter ''
     mkdir -p /btrfs_tmp
-    mount -t btrfs -o subvolid=5 /dev/disk/by-id/virtio-zfsdisk0 /btrfs_tmp
+    mount -t btrfs -o subvolid=5 /dev/disk/by-id/virtio-zfsdisk0-part2 /btrfs_tmp
 
     if [[ -e /btrfs_tmp/@root ]]; then
       mkdir -p /btrfs_tmp/old_roots
@@ -75,11 +78,9 @@
     umount /btrfs_tmp
   '';
 
-  networking.hostName = "nixos";
-  time.timeZone = "Europe/Helsinki";
-
+  # Impermanence
   environment.persistence."/persistent" = {
-    enable = true;  # NB: Defaults to true, not needed
+    enable = true;
     hideMounts = true;
     directories = [
       "/var/log"
@@ -107,11 +108,12 @@
         { directory = ".local/share/keyrings"; mode = "0700"; }
         ".local/share/direnv"
       ];
-      files = [
-        ".screenrc"
-      ];
+      files = [ ".screenrc" ];
     };
   };
+
+  networking.hostName = "nixos";
+  time.timeZone = "Europe/Helsinki";
 
   users.users.root.initialPassword = "root";
   users.users.alukortti = {
